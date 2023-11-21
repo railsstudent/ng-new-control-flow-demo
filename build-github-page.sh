e67d539a7c4f53104d8f7e8a3053a4469aa36ee1
Connie Leung <cleung334@gmail.com>
2023-11-21 22:55:53 +0800

    chore: update build script to deploy to github page

--- build-github-page.sh
+++ build-github-page.sh
@@ -5,4 +5,6 @@ rm -rf docs
 echo "build project starts"
 ng build --output-path docs/
 cp ./docs/browser/index.html  ./docs/browser/404.html
+cp ./docs/browser/*  ./docs
+rm -rf ./docs/browser
 echo 'build project finishes'
